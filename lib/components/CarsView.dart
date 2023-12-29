04273a94e029dbced44b8790d5a43e750b0db9c3
vladimir <rog87@yandex.ru>
2023-12-29 15:11:08 +0300

    главная страница. подвязал данные

---
+++ lib/components/CarsView.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_barcodes/barcodes.dart';
+
+class CreditCardsPage extends StatelessWidget {
+  const CreditCardsPage({super.key, required this.cardNumber, required this.name, required this.secondName, required this.bonusTotal, required this.bonusPromo, required this.barcode});
+  final String cardNumber;
+  final String name;
+  final String secondName;
+  final int bonusTotal;
+  final int bonusPromo;
+  final String barcode;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _buildCreditCard(
+                color: Colors.black,
+                cardNumber: cardNumber,
+                cardHolder: '$name $secondName',
+                bonusTotal: "$bonusTotal",
+                bonusPromo: "$bonusPromo",
+                barcode: barcode,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Build the title section
+  Column _buildTitleSection({@required title, @required subTitle}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0, top: 16.0),
+          child: Text(
+            '$title',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0, bottom: 16.0),
+          child: Text(
+            '$subTitle',
+            style: TextStyle(fontSize: 21, color: Colors.black45),
+          ),
+        )
+      ],
+    );
+  }
+
+  // Build the credit card widget
+  Card _buildCreditCard(
+      {required Color color,
+      required String bonusTotal,
+      required String cardHolder,
+      required String cardNumber,
+      required String bonusPromo,
+      required String barcode}) {
+    return Card(
+      elevation: 4.0,
+      color: color,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(14),
+      ),
+      child: Container(
+        height: 200,
+        padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 22.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            //_buildLogosBlock(),
+            Padding(
+              padding: const EdgeInsets.only(top: 12.0),
+              child:
+              Text.rich(TextSpan(children: [
+                TextSpan(text: 'Доступно бонусов: ', style: TextStyle(color: Colors.white, fontSize: 22, fontFamily: 'CourrierPrime')),
+                TextSpan(text: bonusTotal, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.green)),
+              ],
+              )
+              ),
+              // Text(
+              //   '$cardNumber',
+              //   style: TextStyle(color: Colors.white, fontSize: 22, fontFamily: 'CourrierPrime'),
+              // ),
+            ),
+            bonusPromoWidget(bonusPromo),
+            Center(
+              child: Container(height: 60, width: 200, child: SfBarcodeGenerator(value: barcode, symbology: EAN13(), showValue: true,)),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                _buildDetailsBlock(label: 'Владелец', value: cardHolder),
+                _buildDetailsBlock(label: 'Номер карты', value: cardNumber),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Build the top row containing logos
+  Row _buildLogosBlock() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Image.asset(
+          "assets/images/contact_less.png",
+          height: 20,
+          width: 18,
+        ),
+        Image.asset(
+          "assets/images/mastercard.png",
+          height: 50,
+          width: 50,
+        ),
+      ],
+    );
+  }
+
+// Build Column containing the cardholder and expiration information
+  Column _buildDetailsBlock({required String label, required String value}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          '$label',
+          style: TextStyle(
+              color: Colors.grey, fontSize: 11, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          '$value',
+          style: TextStyle(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+        )
+      ],
+    );
+  }
+
+// Build the FloatingActionButton
+  Container _buildAddCardButton({
+    required Icon icon,
+    required Color color,
+  }) {
+    return Container(
+      margin: const EdgeInsets.only(top: 24.0),
+      alignment: Alignment.center,
+      child: FloatingActionButton(
+        elevation: 2.0,
+        onPressed: () {
+          print("Add a credit card");
+        },
+        backgroundColor: color,
+        mini: false,
+        child: icon,
+      ),
+    );
+  }
+
+  bonusPromoWidget(bonusPromo) {
+      if (bonusPromo != '0') {
+      return Text.rich(TextSpan(children: [
+        TextSpan(text: 'Скоро сгорят: ', style: TextStyle(color: Colors.white, fontSize: 20, fontFamily: 'CourrierPrime')),
+        TextSpan(text: bonusPromo, style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.red)),
+      ],
+      )
+      );
+      } else return SizedBox(height: 1,);
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,177 @@
+import 'dart:convert';
+import 'package:beletag/components/CarsView.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
+class scrHomeScreen extends StatefulWidget {
+  scrHomeScreen();
+
+  @override
+  State<scrHomeScreen> createState() => _scrHomeScreenState();
+}
+
+class _scrHomeScreenState extends State<scrHomeScreen> {
+  bool success = false;
+  String message = '';
+  String cardNumber = '';
+  String name = '';
+  String secondName = '';
+  String barcode = '';
+  String phone = '';
+  String email = '';
+  String gender = 'F';
+  DateTime birthday = DateTime.now();
+  int level = 1;
+  bool blocked = false;
+  bool notify_email = false;
+  bool notify_sms = false;
+
+  int bonus = 0;
+  int bonusTotal = 0;
+  int bonusPromo = 0;
+
+  var resp;
+
+  Future httpGetUserData() async {
+    var _url=Uri(path: '/c/beletag_bonus/hs/v1/user/89229220315/', host: 's4.rntx.ru', scheme: 'https');
+    var _headers = <String, String> {
+      'Accept': 'application/json',
+      'Authorization': 'Basic YWNlOkF4V3lJdnJBS1prdzY2UzdTMEJP'
+    };
+    try {
+      var response = await http.get(_url, headers: _headers);
+      if (response.statusCode == 200) {
+        var notesJson = json.decode(response.body);
+        success = notesJson['success'] ?? false;
+        message = notesJson['message'] ?? '';
+        resp = notesJson['response'] ?? '';
+        cardNumber = resp['card_number'];
+        name = resp['name'];
+        secondName = resp['second_name'];
+        barcode = resp['ean'];
+        phone = resp['phone'];
+        email = resp['email'];
+        level = resp['level'];
+        gender = resp['gender'];
+
+        blocked = resp['blocked'];
+        notify_email = resp['notify_email'];
+        notify_sms = resp['notify_sms'];
+
+        birthday = DateTime.parse(resp['birthday'].toString()) as DateTime;
+
+        print('ответ от сервера: ' + birthday.toString());
+
+      }
+    } catch (error) {
+      print("Ошибка при формировании списка: $error");
+    }
+  }
+
+  Future httpGetUserBonusBalance() async {
+    var _url=Uri(path: '/c/beletag_bonus/hs/v1/balanceall/89229220315/', host: 's4.rntx.ru', scheme: 'https');
+    var _headers = <String, String> {
+      'Accept': 'application/json',
+      'Authorization': 'Basic YWNlOkF4V3lJdnJBS1prdzY2UzdTMEJP'
+    };
+    try {
+      var response = await http.get(_url, headers: _headers);
+      if (response.statusCode == 200) {
+        var notesJson = json.decode(response.body);
+        success = notesJson['success'] ?? false;
+        message = notesJson['message'] ?? '';
+        resp = notesJson['response'] ?? '';
+        bonusTotal = resp['Total'];
+        bonusPromo = resp['OfThemPromo'];
+        print('Баланс' + bonusTotal.toString());
+      }
+    } catch (error) {
+      print("Ошибка при формировании списка: $error");
+    }
+  }
+
+
+  @override
+  void initState() {
+    httpGetUserBonusBalance();
+    httpGetUserData().then((value) {
+      setState(() {
+      });
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('CleverWear'),
+          centerTitle: true,
+          //backgroundColor: Colors.grey[900],
+          //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          //actions: [IconButton(onPressed: () {}, icon: Icon(Icons.menu))],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CreditCardsPage(cardNumber: cardNumber, name: name, secondName: secondName, bonusTotal: bonusTotal, bonusPromo: bonusPromo, barcode: barcode),
+              Card(
+                child: ListTile(
+                  title: Text('История покупок', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),),
+                  subtitle: Text('Чеки, возвраты, начисления бонусов'),
+                  leading: Icon(Icons.currency_ruble_rounded),
+                  //trailing: Text('100', style: TextStyle(fontSize: 18, color: Colors.green),),
+                  onTap: () async {
+                    //final result =
+                    //    await Navigator.pushNamed(context, '/cashHome', arguments: {'summa': 100});
+                  },
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  title: Text('Мои данные', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),),
+                  subtitle: Text('Персональные данные'),
+                  leading: Icon(Icons.account_circle),
+                  //trailing: Text('100', style: TextStyle(fontSize: 18, color: Colors.green),),
+                  onTap: () async {
+                    //final result =
+                    //    await Navigator.pushNamed(context, '/cashHome', arguments: {'summa': 100});
+                  },
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  title: Text('Магазины', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),),
+                  subtitle: Text('Адреса магазинов'),
+                  leading: Icon(Icons.maps_home_work_sharp),
+                  //trailing: Text('100', style: TextStyle(fontSize: 18, color: Colors.green),),
+                  onTap: () async {
+                    //final result =
+                    //    await Navigator.pushNamed(context, '/cashHome', arguments: {'summa': 100});
+                  },
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  title: Text('О нас', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),),
+                  subtitle: Text('Информация'),
+                  leading: Icon(Icons.info_outlined),
+                  //trailing: Text('100', style: TextStyle(fontSize: 18, color: Colors.green),),
+                  onTap: () async {
+                    //final result =
+                    //    await Navigator.pushNamed(context, '/cashHome', arguments: {'summa': 100});
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {},
+        //   child: Text('+'),
+        // )
+        //backgroundColor: Colors.grey[900]),
+        );
+  }
+}
